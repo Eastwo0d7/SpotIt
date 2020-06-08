e7e36b8b2a32db079cbf1a13b25ab1b8a557d018
Brett LeBaron <brett.lebaron@gmail.com>
2020-06-08 19:47:33 -0400

    updated to cropped images

--- cards.js
+++ cards.js
@@ -1,24 +1,24 @@
 function playGame(){
     var cards = [
-        "images/20200515_121628.jpg","images/20200515_121633.jpg","images/20200515_121639.jpg",
-        "images/20200515_121645.jpg","images/20200515_121650.jpg","images/20200515_121655.jpg",
-        "images/20200515_121700.jpg","images/20200515_121705.jpg","images/20200515_121709.jpg",
-        "images/20200515_121724.jpg","images/20200515_121729.jpg","images/20200515_121733.jpg",
-        "images/20200515_121740.jpg","images/20200515_121745.jpg","images/20200515_121750.jpg",
-        "images/20200515_121754.jpg","images/20200515_121800.jpg","images/20200515_121807.jpg",
-        "images/20200515_121812.jpg","images/20200515_121816.jpg","images/20200515_121821.jpg",
-        "images/20200515_121827.jpg","images/20200515_121832.jpg","images/20200515_121837.jpg",
-        "images/20200515_121841.jpg","images/20200515_121846.jpg","images/20200515_121851.jpg",
-        "images/20200515_121856.jpg","images/20200515_121904.jpg","images/20200515_121910.jpg",
-        "images/20200515_121916.jpg","images/20200515_121921.jpg","images/20200515_121926.jpg",
-        "images/20200515_121931.jpg","images/20200515_121936.jpg","images/20200515_121940.jpg",
-        "images/20200515_121945.jpg","images/20200515_121950.jpg","images/20200515_121955.jpg",
-        "images/20200515_122001.jpg","images/20200515_122006.jpg","images/20200515_122011.jpg",
-        "images/20200515_122016.jpg","images/20200515_122021.jpg","images/20200515_122026.jpg",
-        "images/20200515_122033.jpg","images/20200515_122040.jpg","images/20200515_122046.jpg",
-        "images/20200515_122050.jpg","images/20200515_122055.jpg","images/20200515_122100.jpg",
-        "images/20200515_122105.jpg","images/20200515_122110.jpg","images/20200515_122116.jpg",
-        "images/20200515_122120.jpg"
+        "images/20200520_112602.jpg","images/20200520_112623.jpg","images/20200520_112637.jpg",
+        "images/20200520_112649.jpg","images/20200520_112716.jpg","images/20200520_112731.jpg",
+        "images/20200520_112745.jpg","images/20200520_112757.jpg","images/20200520_112810.jpg",
+        "images/20200520_112821.jpg","images/20200520_112834.jpg","images/20200520_112844.jpg",
+        "images/20200520_112857.jpg","images/20200520_112952.jpg","images/20200520_113001.jpg",
+        "images/20200520_113011.jpg","images/20200520_113022.jpg","images/20200520_113032.jpg",
+        "images/20200520_113040.jpg","images/20200520_113049.jpg","images/20200520_113058.jpg",
+        "images/20200520_113108.jpg","images/20200520_113128.jpg","images/20200520_113150.jpg",
+        "images/20200520_113203.jpg","images/20200520_113217.jpg","images/20200520_113232.jpg",
+        "images/20200520_113247.jpg","images/20200520_113257.jpg","images/20200520_113314.jpg",
+        "images/20200520_113329.jpg","images/20200520_113346.jpg","images/20200520_113405.jpg",
+        "images/20200520_113422.jpg","images/20200520_113432.jpg","images/20200520_113444.jpg",
+        "images/20200520_113452.jpg","images/20200520_113505.jpg","images/20200520_113514.jpg",
+        "images/20200520_113528.jpg","images/20200520_113538.jpg","images/20200520_113550.jpg",
+        "images/20200520_113559.jpg","images/20200520_113607.jpg","images/20200520_113616.jpg",
+        "images/20200520_113624.jpg","images/20200520_113639.jpg","images/20200520_113648.jpg",
+        "images/20200520_113700.jpg","images/20200520_113709.jpg","images/20200520_113718.jpg",
+        "images/20200520_113727.jpg","images/20200520_113735.jpg","images/20200520_113743.jpg",
+        "images/20200520_113753.jpg"
     ];
     var card1 = cards[Math.floor(Math.random() * cards.length)];
     var card2 = cards[Math.floor(Math.random() * cards.length)];
